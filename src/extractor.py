2c6d8ead04b4a379587af64fdeeaf13ec53592b7
CodeSlavve <vsangeeta045@gmail.com>
2026-02-20 21:59:16 +0530

    Project done? got to do a loooot of Preprocessing

--- src/extractor.py
+++ src/extractor.py
@@ -2,8 +2,14 @@ import re
 from urllib.parse import urlparse
 import pandas as pd
 
+## 1 -> Phishing /// 0 -> Legitimate 
+
+shortners = pd.read_csv('data/shortners.csv')
+
+# Address Bar Based Features
+
 def url_length(url):
-    if len(url)>=52:
+    if len(url)>=54:
         return 1
     return 0
 
@@ -29,11 +35,26 @@ def no_of_subdomains(url):
     return 0
 
 def has_ip(url):
-    parsed = urlparse(url)
-    hostname = parsed.netloc
+    hostname = urlparse(url).netloc
     if re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',hostname):
         return 1
     return 0
+
+def shortening_service(url):
+    return 1 if any(s in url for s in shortners) else 0
+
+def double_slash_redirect(url):
+    return 1 if url.count('//') > 1 else 0
+
+def ssl_final_state(url):
+    return int(not url.startswith("https://"))
+
+def https_token(url):
+    hostname = urlparse(url).netloc.lower()
+    return int("https" in hostname)
+
+def redirect_symbol(url):
+    return int('//' in url[8:])
 
 data = pd.read_csv('data/raw_dataset.csv')
 columns = pd.DataFrame({
@@ -42,7 +63,13 @@ columns = pd.DataFrame({
     'has_dash' : data['url'].apply(has_dash),
     'dot' : data['url'].apply(dot),
     'has_ip': data['url'].apply(has_ip),
+    'shortening_service' : data['url'].apply(shortening_service),
+    'double_slash_redirect' : data['url'].apply(double_slash_redirect),
+    'ssl_final_state' : data['url'].apply(ssl_final_state),
+    'https_token' : data['url'].apply(https_token),
+    'redirect_symbol' : data['url'].apply(redirect_symbol),
     'label' : data['label']
 })
-columns.to_csv('data/processed_dataset.csv',index=False)
+
+columns.to_csv('data/processed_dataset_v2.csv',index=False)
 print("Raw data is processed successfully...")
--- src/train.py
+++ src/train.py
@@ -1,10 +1,12 @@
 import pandas as pd
+import joblib 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
-data = pd.read_csv('data/processed_dataset.csv')
+data = pd.read_csv('data/processed_dataset_v2.csv')
 X = data.drop(['label'],axis=1)
 y = data['label']
 
@@ -14,23 +16,33 @@ X_train, X_test, y_train, y_test = train_test_split(
     random_state=42
 )
 
+# Logistic Regression
 lr = LogisticRegression(max_iter=1000)
 lr.fit(X_train, y_train)
+joblib.dump(lr,'models/logistic_v2.pkl')
 
 lr_pred = lr.predict(X_test)
-
 accuracy = accuracy_score(y_test, lr_pred)
 print(f"LogisticRegression Accuracy: {accuracy * 100:.2f}%")
 print("\nLogisticRegression Classification Report:\n", classification_report(y_test, lr_pred))
 
+#Random Forest
 rf = RandomForestClassifier(n_estimators= 100)
 rf.fit(X_train,y_train)
+joblib.dump(rf,'models/random_forest_v2.pkl')
 
 rf_pred = rf.predict(X_test)
 print(f"RandomForest Accuracy: {accuracy_score(y_test, rf_pred) * 100:.2f}%")
 print("\nRandomForestClassification Report:\n", classification_report(y_test, rf_pred))
 
+# SVM
+svm = SVC(kernel='rbf')
+svm.fit(X_train, y_train)
+joblib.dump(svm,'models/svm_v2.pkl')
 
+svm_pred = svm.predict(X_test)
+print(f"SVM Accuracy: {accuracy_score(y_test, svm_pred) * 100:.2f}%")
+print("\nSVM Classification Report:\n", classification_report(y_test, svm_pred))
 
 #ChatGpt
 import matplotlib.pyplot as plt
@@ -42,6 +54,8 @@ sns.heatmap(lrcm, annot=True, fmt='d', cmap='Blues')
 plt.xlabel('Predicted')
 plt.ylabel('Actual')
 plt.title('Confusion Matrix for LogisticRegression')
+plt.savefig("models/result/v2/logistic_v2.png")
+plt.close()
 
 rfcm = confusion_matrix(y_test, rf_pred)
 plt.figure(figsize=(6,4))
@@ -49,4 +63,14 @@ sns.heatmap(rfcm, annot=True, fmt='d', cmap='Blues')
 plt.xlabel('Predicted')
 plt.ylabel('Actual')
 plt.title('Confusion Matrix for RandomForest')
-plt.show()
+plt.savefig("models/result/v2/random_forest_v2.png")
+plt.close()
+
+svmcm = confusion_matrix(y_test, svm_pred)
+plt.figure(figsize=(6,4))
+sns.heatmap(svmcm, annot=True, fmt='d', cmap='Blues')
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
+plt.title('Confusion Matrix for SVM')
+plt.savefig("models/result/v2/svm_v2.png")
+plt.close()
